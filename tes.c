72905e6dcf3ab25f588456aa732a99183d38690f
AHMAD SHONHAJI <88180498+ahmadsoni@users.noreply.github.com>
2021-10-01 18:17:48 +0700

    update Konversi Angka ke Hari (hacktoberfast)
    update Konversi Angka ke Hari

--- tes.c
+++ tes.c
@@ -2,82 +2,37 @@
 
 #include <stdio.h>
 main()
-{
-    float totalharga;
-    float jumlahHarga = 0.0;
-    int lagi = 1;
+{     int angka;
+    
     while (lagi > 0)
     {
-        printf("Daftar barang : \n");
-        printf("1. Penghapus --> Rp 2.000 \n");
-        printf("2. Pensil --> Rp. 3.000 \n");
-        printf("3. Penggaris --> Rp 3.000 \n");
-        printf("4. Pulpen -> 4.000 \n");
-        int pilihan;
-        printf("Mau beli yang mana?: ");
-        scanf("%d", &pilihan);
+    
+    puts("-------------------------------------");
+    puts("\t Konversi Angka ke Hari");
+    puts("-------------------------------------");
+    printf("Masukkan angka : ");
+    scanf("%d", &angka);
 
-        int jumlahBarang;
-        printf("Jumlahnya berapa: ");
-        scanf(" %d", &jumlahBarang);
-
-        float diskon = 10.0 / 100.0; //diskon 10%
-        float potongan;
-
-        if(pilihan == 1) {
-            if(jumlahBarang >= 3) {
-                jumlahHarga += 2000;
-                jumlahHarga *= jumlahBarang;
-                potongan = jumlahHarga * diskon;
-                jumlahHarga -= potongan;
-            } else {
-                jumlahHarga += 2000;
-                jumlahHarga *= jumlahBarang;
-            }
-        }
-
-        if(pilihan == 2) {
-             if(jumlahBarang >= 3) {
-                jumlahHarga += 3000;
-                jumlahHarga *= jumlahBarang;
-                potongan = jumlahHarga * diskon;
-                jumlahHarga -= potongan;
-            } else {
-                jumlahHarga += 3000;
-                jumlahHarga *= jumlahBarang;
-            }
-        }
-
-        if(pilihan == 3) {
-            if(jumlahBarang >= 3) {
-                jumlahHarga += 3000;
-                jumlahHarga *= jumlahBarang;
-                potongan = jumlahHarga * diskon;
-                jumlahHarga -= potongan;
-            } else {
-                jumlahHarga += 3000;
-                jumlahHarga *= jumlahBarang;
-            }
-        }
-
-        if(pilihan == 4) {
-             if(jumlahBarang >= 3) {
-                jumlahHarga += 4000;
-                jumlahHarga *= jumlahBarang;
-                potongan = jumlahHarga * diskon;
-                jumlahHarga -= potongan;
-            } else {
-                jumlahHarga += 4000;
-                jumlahHarga *= jumlahBarang;
-            }
-        }
-
-        totalharga += jumlahHarga;
-        jumlahHarga = 0;
-        printf("Mau beli lagi: (1 =yes , 0=no)");
-        scanf("%d", &lagi);
+    switch(angka)
+    {
+        case 1 : puts("Minggu");
+            break;
+        case 2 : puts("Senin");
+            break;
+        case 3 : puts("Selasa");
+            break;
+        case 4 : puts("Rabu");
+            break;
+        case 5 : puts("Kamis");
+            break;
+        case 6 : puts("Jumat");
+            break;
+        case 7 : puts("Sabtu");
+            break;
+        default  : puts("Hari tidak Valid");
     }
+    }
+ return 0;
 
-    printf("Total yang harus kamu bayar adalah: %.2f", totalharga);
 
 }
