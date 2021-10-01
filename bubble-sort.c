806c9bca27c2e9ed4fcdba0fe88a0a8a3019b0c5
iambigyandahal <iambigyandahal@gmail.com>
2021-10-01 19:42:50 +0545

    Bubble sort

---
+++ bubble-sort.c
@@ -0,0 +1,47 @@
+// Bubble sort
+
+// Includes
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prototyping of function
+void sort(int arr[], int arrSize);
+void printArr(int arr[], int arrSize);
+void swap(int *a, int *b);
+
+// Main function
+int main() {
+	int arr[] = {10,1,5,9,3,2,76};
+	int arrSize = sizeof(arr)/sizeof(arr[0]);
+	sort(arr, arrSize);
+	printArr(arr, arrSize);
+	return 0;
+}
+
+// swapping function
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+// Main sorting array
+void sort(int arr[], int arrSize) {
+	int a=0, b=0, temp=0;
+	for(a=0;a<arrSize-1;a++) {
+		for(b=0;b<arrSize-a-1;b++) {
+			if(arr[b] > arr[b+1]) {
+				swap(&arr[b], &arr[b+1]);
+			}
+		}
+	}
+}
+
+// Array Printing function
+void printArr(int arr[], int arrSize) {
+	int a=0;
+	for(a=0;a<arrSize;a++) {
+		printf("%d ", arr[a]);
+	}
+	printf("\n");
+}
